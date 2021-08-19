7fa59621b82486de4ea084b53d31555762651bee
gustavorr <gustavo@constata.io>
2021-08-19 14:37:37 -0300

    add category in memory database for category

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Category } from '../src/app/pages/categories/shared/category.model'
+import {switchMap} from 'rxjs/operators'; 
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.scss']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked{
 
   constructor() { }
 
+  currentAction: string = '';
+  categoryForm: FormGroup;
+  pageTitle: string = '';
+  serverErrorMessage: string[] = [];
+  submittingForm: boolean = false;
+  category: Category = new Category();
+
   ngOnInit(): void {
+    this.pageTitle ='test';
   }
 
+  ngAfterContentChecked() {
+  }
 }
