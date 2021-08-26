c60dfb3b58ef494300f3b418d682fef1bfcf23d6
gustavorr <gustavo@constata.io>
2021-08-26 11:50:27 -0300

    adding edit lancamento

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule,FormGroup, FormsModule } from '@angular/forms'; 
 import { EntriesRoutingModule } from './entries-routing.module';
-
+import { EntryFormComponent } from './entry-form/entry-form.component';
 import { EntryListComponent } from './entry-list/entry-list.component';
 
 @NgModule({
-  declarations: [EntryListComponent],
+  
   imports: [
     CommonModule,
     EntriesRoutingModule,
-  ]
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  declarations: [EntryListComponent,EntryFormComponent],
 })
 export class EntriesModule { }
